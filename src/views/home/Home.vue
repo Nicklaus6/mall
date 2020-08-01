08c29f9d76366ca718f52477de0e1287a5f65d99
Klaus <liuminjie_1108@163.com>
2020-08-01 13:49:36 +0800

    'TabControl吸顶问题'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,13 +5,72 @@
     </nav-bar>
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class='tab-control'
+                 :titles=" ['流行','新款','精选']" />
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
+
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import TabControl from 'components/content/tabControl/TabControl'
+import NavBar from 'components/common/navbar/NavBar'
 
 import { getHomeMultidata } from 'network/home'
 
@@ -19,9 +78,12 @@ import { getHomeMultidata } from 'network/home'
 export default {
   name: 'Home',
   components: {
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+
+    TabControl,
+    NavBar,
   },
   data () {
     return {
@@ -42,8 +104,18 @@ export default {
 </script>
 
 <style>
+#home {
+  padding-bottom: 800px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
